90ca19c71e08cc9c2435a34b69e6028ebfaa9906
Ayush Kumar Gupta <76941695+Ayush-923@users.noreply.github.com>
2023-07-07 23:06:11 +0530

    Time: 8 ms (99.38%), Space: 43.5 MB (80.63%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
@@ -1,23 +1,21 @@
 class Solution {
+
     public int maxConsecutiveAnswers(String answerKey, int k) {
-        int maxSize = k;
-        Map<Character, Integer> count = new HashMap<>();
-        for (int i = 0; i < k; i++) {
-            count.put(answerKey.charAt(i), count.getOrDefault(answerKey.charAt(i), 0) + 1);
-        }
-        
-        int left = 0;
-        for (int right = k; right < answerKey.length(); right++) {
-            count.put(answerKey.charAt(right), count.getOrDefault(answerKey.charAt(right), 0) + 1);
-            
-            while (Math.min(count.getOrDefault('T', 0), count.getOrDefault('F', 0)) > k) {
-                count.put(answerKey.charAt(left), count.get(answerKey.charAt(left)) - 1);
-                left++;
+
+        int result = 0;
+        int maxFreq = 0;
+        int[] counts = new int[128];
+        for(int i = 0; i < answerKey.length(); i++) {
+            maxFreq = Math.max(maxFreq, ++counts[answerKey.charAt(i)]);
+            if(result - maxFreq < k) {
+                result++;
+            } else {
+                counts[answerKey.charAt(i - result)]--;
             }
-            
-            maxSize = Math.max(maxSize, right - left + 1);
         }
-                    
-        return maxSize;
+
+        return result;
+
     }
+
 }
