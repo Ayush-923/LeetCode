424386d2d199bc894f633817f16b4f9a1b894697
Ayush Kumar Gupta <76941695+Ayush-923@users.noreply.github.com>
2023-07-09 20:10:40 +0530

    Time: 18 ms (100.00%), Space: 42.4 MB (59.01%) - LeetHub

--- 2272-substring-with-largest-variance/2272-substring-with-largest-variance.java
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.java
@@ -1,49 +1,26 @@
 class Solution {
     public int largestVariance(String s) {
-        int[] counter = new int[26];
-        for (char ch : s.toCharArray()) {
-            counter[(int)(ch - 'a')]++;
+        int output = 0;
+        int[][] dif = new int[26][26];
+        int[][] difB = new int [26][26];
+        for(int i = 0; i < 26; i++){
+            Arrays.fill(difB[i], -s.length());
         }
-        int globalMax = 0;
-        
-        for (int i = 0; i < 26; i++) {
-            for (int j = 0; j < 26; j++) {
-                // major and minor cannot be the same, and both must appear in s.
-                if (i == j || counter[i] == 0 || counter[j] == 0) {
-                    continue;
-                }
-                
-                // Find the maximum variance of major - minor.        
-                char major = (char)('a' + i);
-                char minor = (char)('a' + j);
-                int majorCount = 0;
-                int minorCount = 0;
-                
-                // The remaining minor in the rest of s.
-                int restMinor = counter[j];
-                
-                for (char ch : s.toCharArray()) {    
-                    if(ch == major) {
-                        majorCount++;
-                    }
-                    if(ch == minor) {
-                        minorCount++;
-                        restMinor--;
-                    }
-                    
-                    // Only update the variance if there is at least one minor.
-                    if (minorCount > 0)
-                        globalMax = Math.max(globalMax, majorCount - minorCount);
-                    
-                    // We can discard the previous string if there is at least one remaining minor.
-                    if (majorCount < minorCount && restMinor > 0) {
-                        majorCount = 0;
-                        minorCount = 0;
-                    }
-                }
+
+        for(int j = 0; j < s.length(); j++){
+            int ch = s.charAt(j) - 'a';
+            for(int k = 0; k < 26; k++){
+                if(k == ch) continue;
+                ++dif[ch][k];
+                ++difB[ch][k];
+                difB[k][ch] = --dif[k][ch];
+                dif[k][ch] = Math.max(dif[k][ch], 0);
+                output = Math.max(output, Math.max(difB[ch][k], difB[k][ch]));
+
             }
         }
-        
-        return globalMax;
+
+        return output;
+
     }
 }
